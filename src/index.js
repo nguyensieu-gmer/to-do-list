b95a2c6fc0043b807d222decc854763c9fe485d0
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-04-17 10:32:05 +0700

    Change all sidebar element to button
    Except user

--- src/index.js
+++ src/index.js
@@ -3,41 +3,28 @@ import { makeTask } from './independencies/task.js';
 import { addDays, addHours, format } from 'date-fns';
 import './style.css';
 
-const MP = ManageProject();
+function addDefault(){
+    const MP = ManageProject();
+    const project = 'work out';
+    const todo = 'kalisthenic';
+    MP.addProject(project);
+    MP.addTodo(project, todo);
+    const task1 = makeTask('push up', '12:30', true, false);
+    const task2 = makeTask('crunch', '10:00', true, false);
+    const task3 = makeTask('squat', '13:30', true, false);
+    MP.addTask(project, todo, task1);
+    MP.addTask(project, todo, task2);
+    MP.addTask(project, todo, task3);
+    localStorage.setItem("todolist", JSON.stringify(MP.getprojects()));
+}
 
-const project = 'work out';
-const todo = 'kalisthenic';
-MP.addProject(project);
-MP.addTodo(project, todo);
-const task1 = makeTask('push up', '12:30', true, false);
-const task2 = makeTask('crunch', '10:00', true, false);
-const task3 = makeTask('squat', '13:30', true, false);
-MP.addTask(project, todo, task1);
-MP.addTask(project, todo, task2);
-MP.addTask(project, todo, task3);
+addDefault();
 
-localStorage.setItem("todolist", JSON.stringify(MP.getprojects()));
+let todolist = JSON.parse(localStorage.getItem('todolist'));
+console.log(todolist);
 
-let projects = JSON.parse(localStorage.getItem('todolist'));
-console.log(projects);
-
-// const container = document.querySelector('.container');
-// projects.forEach(item => {
-//     const div = document.createElement('div');
-//     const h1 = document.createElement('h1');
-//     h1.textContent = `${item.project}`;
-//     div.appendChild(h1);
-//     item.todos.forEach(t => {
-//         const h2 = document.createElement('h2');
-//         h2.textContent = `${t.todo}`;
-//         div.appendChild(h2);
-//         const ul = document.createElement('ul');
-//         t.tasks.forEach(ts => {
-//             const li = document.createElement('li');
-//             li.textContent = `${ts.title}`;
-//             ul.appendChild(li);
-//         })
-//         div.appendChild(ul);
-//     })
-//     container.appendChild(div);
-// })
+const projectsUi = document.getElementById('projects');
+projects.forEach(p => {
+    const div = document.createElement('div');
+    div.classList.add('sidebar_item');
+});
