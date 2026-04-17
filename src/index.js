f5ee71067b39c729eae01cfbde72b548f06aaa7c
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-04-17 14:54:08 +0700

    Add default

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ const PM = ProjectManager();
 const TM = TodoManager();
 const TKM = TaskManager();
 
+function addDefault(){
+    const projectId = PM.addProject('Work out');
+    const project = PM.findProjectById(projectId);
+    const todoId = TM.addTodo(project, 'calisthenic');
+    const findTodo = TM.findTodoById(project, todoId);
+    const task = TKM.makeTask('push up', '12:00', true, false);
+    TM.addTask(findTodo, task);
+    localStorage.setItem('todolist', JSON.stringify(PM.getprojects()));
+}
+
+addDefault();
