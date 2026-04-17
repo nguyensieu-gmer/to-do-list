fb353a9096511f67e2fb52e35dd5094f86d73530
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-04-17 13:01:28 +0700

    Change project

--- src/independencies/project.js
+++ src/independencies/project.js
@@ -1,67 +1,78 @@
-export default function(){
+export function ManageProject(){
     let projects = []; // project must doesn't have undefined
+    //{project: projectName, id: crypto.randomUUID(), todos: []}
+    //{todo: todoName,id: crypto.randomUUID(), tasks: []}
 
     // undefined if not found and ob if contrast
-    const findProject = (projectName) => {
-        const project = projects.find(ob => ob.project === projectName);
+    const findProject = (projectId) => {
+        const project = projects.find(ob => ob.id === projectId);
         return project; 
     }
 
-    // undefined if not found and ob if contrast
-    const findTodo = (projectName, todoName) => {
-        const project = findProject(projectName);
-        if (project === undefined) return;
-        const todo = project.todos.find(ob => ob.todo === todoName);
-        return todo; 
-    }
-
-    const renameProject = (projectName, newProjectName) => {
-        const targetProject = findProject(projectName);
+    const renameProject = (projectId, newProjectName) => {
+        const targetProject = findProject(projectId);
         if (targetProject === undefined) 
             return; // projectName not be found
         targetProject.project = newProjectName;
     }
 
-    const renameTodo = (projectName, todoName, newTodoName) => {
-        const targetTodo = findTodo(projectName, todoName);
+    // not able duplicate project name
+    const addProject = (projectName) => {
+        const project = {project: projectName, id: crypto.randomUUID(), todos: []};
+        projects.push(project);
+        return project.id;
+    }
+
+    const deleteProject = (projectId) => {
+        projects = projects.filter(item => item.id !== projectId);
+    }
+
+    // undefined if not found and ob if contrast
+    const findTodo = (projectId, todoId) => {
+        const project = findProject(projectId);
+        if (project === undefined) return;
+        const todo = project.todos.find(ob => ob.id === todoId);
+        return todo; 
+    }
+
+    const renameTodo = (projectId, todoId, newTodoName) => {
+        const targetTodo = findTodo(projectId, todoId);
         if (targetTodo === undefined)
             return; // todoName not be found
         targetTodo.todo = newTodoName;
     }
 
-    // not able duplicate project name
-    const addProject = (projectName) => {
-        //duplicate project name case
-        if (findProject(projectName) !== undefined) return;
-        projects.push({project: projectName, todos: []});
+    // not be able to duplicate todo name
+    const addTodo = (projectId, todoName) => {
+        const project = findProject(projectId);
+        if (project === undefined) 
+            return; // Project with project id isn't be found
+        let todo = {todo: todoName,id: crypto.randomUUID(), tasks: []};
+        project.todos.push(todo);
+        return todo.id;
     }
 
-    // not be able to duplicate todo name
-    const addTodo = (projectName, todoName) => {
-        const project = findProject(projectName);
-        if (project === undefined) 
-            return; // Project with projectName isn't be found
-        if (findTodo(projectName, todoName) !== undefined){
-            return; // duplicate todo
-        }
-        project.todos.push({todo: todoName, tasks: []});
+    const deleteTodo = (projectId, todoId) => {
+        let project = findProject(projectId);
+        if (!project) return;
+        project.todos = project.todos.filter(item => item.id !== todoId);
     }
 
     // able to duplicate task
-    const addTask = (projectName, todoName, task) => {
-        const project = findProject(projectName);
+    const addTask = (projectId, todoId, task) => {
+        const project = findProject(projectId);
         if (project === undefined){
             return;// Project with projectName isn't be found
         }
-        const todo = findTodo(projectName, todoName);
+        const todo = findTodo(projectId, todoId);
         if (todo === undefined){
             return;//todo with todoName isn't be found
         }
         todo.tasks.push(task);
     }
 
-    const findTaskById = (projectName, todoName, taskId) => {
-        const todo = findTodo(projectName, todoName);
+    const findTaskById = (projectId, todoId, taskId) => {
+        const todo = findTodo(projectId, todoId);
         if (!todo) return; // not found todo
         return todo.tasks.find(item => item.id === taskId);
     }
@@ -79,18 +90,8 @@ export default function(){
         Object.assign(task, newData);
     }
 
-    const deleteProject = (projectName) => {
-        projects = projects.filter(item => item.project !== projectName);
-    }
-
-    const deleteTodo = (projectName, todoName) => {
-        let project = findProject(projectName);
-        if (!project) return;
-        project.todos = project.todos.filter(item => item.todo !== todoName);
-    }
-
-    const deleteTask = (projectName, todoName, taskId) => {
-        let todo = findTodo(projectName, todoName);
+    const deleteTask = (projectId, todoId, taskId) => {
+        let todo = findTodo(projectId, todoId);
         if (!todo) return;
         todo.tasks = todo.tasks.filter(item => item.id !== taskId);
     }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ManageProject from './independencies/project.js';
+import { ManageProject } from './independencies/project.js';
 import { makeTask } from './independencies/task.js';
 import { addDays, addHours, format } from 'date-fns';
 import './style.css';
@@ -7,24 +7,37 @@ function addDefault(){
     const MP = ManageProject();
     const project = 'work out';
     const todo = 'kalisthenic';
-    MP.addProject(project);
-    MP.addTodo(project, todo);
+    let pId = MP.addProject(project);
+    let tId = MP.addTodo(pId, todo);
     const task1 = makeTask('push up', '12:30', true, false);
     const task2 = makeTask('crunch', '10:00', true, false);
     const task3 = makeTask('squat', '13:30', true, false);
-    MP.addTask(project, todo, task1);
-    MP.addTask(project, todo, task2);
-    MP.addTask(project, todo, task3);
-    localStorage.setItem("todolist", JSON.stringify(MP.getprojects()));
+    MP.addTask(pId, tId, task1);
+    MP.addTask(pId, tId, task2);
+    MP.addTask(pId, tId, task3);
+
+    console.log(MP.getprojects())
+    // localStorage.setItem("todolist", JSON.stringify(MP.getprojects()));
 }
 
 addDefault();
 
-let todolist = JSON.parse(localStorage.getItem('todolist'));
-console.log(todolist);
+// let projectList = JSON.parse(localStorage.getItem('todolist'));
+// console.log(projectList);
 
-const projectsUi = document.getElementById('projects');
-projects.forEach(p => {
-    const div = document.createElement('div');
-    div.classList.add('sidebar_item');
-});
+// function addProjectNameToSideBar(){
+//     const projectsUi = document.getElementById('projects');
+//     projectList.forEach(project => {
+//         const btn = document.createElement('button');
+//         btn.classList.add('sidebar_item');
+//         const i = document.createElement('i');
+//         i.classList.add('ri-task-line');
+//         const para = document.createElement('p');
+//         para.textContent = `${project.project}`;
+//         btn.appendChild(i);
+//         btn.appendChild(para);
+//         projectsUi.appendChild(btn);
+//     });
+// }
+
+// addProjectNameToSideBar();
